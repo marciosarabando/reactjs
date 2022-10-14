fba06e9541ae241f27efab5351027d34fa678103
Marcio Sarabando <marciosarabando@iMac-de-Marcio.local>
2022-10-14 16:50:10 -0300

    adjustment scripts

--- 02-Boilerplate/scripts/deploy.sh
+++ 02-Boilerplate/scripts/deploy.sh
@@ -7,8 +7,10 @@ sh scripts/build.sh
 
 # minify js
 minify bundle.js > __deployme/bundle.js
+
 # minify css
 minify bundle.css > __deployme/bundle.css
+
 # copy html and images
 cp index.html __deployme/index.html
 cp -r images/ __deployme/images/
