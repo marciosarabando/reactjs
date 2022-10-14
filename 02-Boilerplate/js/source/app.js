3563c120e4aaa9d0f281a35f9626543e05b16c99
Marcio Sarabando <marciosarabando@iMac-de-Marcio.local>
2022-10-14 15:20:02 -0300

    46% do livro

---
+++ 02-Boilerplate/js/source/app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Logo from './components/Logo';
+
+ReactDOM.render(
+    <h1>
+        <Logo /> Oi Mozaom! Te amo! Voce o Lucca e a Sofia
+    </h1>,
+    document.getElementById('app')
+);
